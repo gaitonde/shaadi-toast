d58311245f4bc3ea599ea536bbfd851072f0df5c
dayal <dayal.gaitonde@gmail.com>
2024-09-25 15:01:49 -0700

    move font location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,12 @@ import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../../public/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../../public/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });

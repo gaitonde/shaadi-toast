31f99099198faee0044afce995c9c08a373d418a
sparasnis <shaan@turnkeyanalytics.io>
2024-09-27 15:47:10 -0700

    fix policy page

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -33,21 +33,21 @@ export default function PrivacyPolicy() {
               <p className="font-bold">Last Updated: 09/10/2024</p>
               
               <p>
-                GreenPen AI Labs, Inc. doing business as ShaadiToast ("ShaadiToast," "we," "us," and "our") provides an AI-powered tool to assist our customers in writing wedding speeches. This Privacy Policy is designed to help you understand how we collect, use, and share your personal information and to help you understand and exercise your privacy rights.
+                GreenPen AI Labs, Inc. doing business as ShaadiToast (&quot;ShaadiToast,&quot; &quot;we,&quot; &quot;us,&quot; and &quot;our&quot;) provides an AI-powered tool to assist our customers in writing wedding speeches. This Privacy Policy is designed to help you understand how we collect, use, and share your personal information and to help you understand and exercise your privacy rights.
               </p>
 
               <h2 className={`text-xl font-bold text-[#8b0000] mt-4 ${merriweather.className}`}>
                 Scope
               </h2>
               <p>
-                This Privacy Policy applies to personal information processed by us on our website, our services offered via the website, and our related online and offline offerings. To make this Privacy Policy easier to read, our websites, our services offered via the website, and our related offerings are collectively called "Services."
+                This Privacy Policy applies to personal information processed by us on our website, our services offered via the website, and our related online and offline offerings. To make this Privacy Policy easier to read, our websites, our services offered via the website, and our related offerings are collectively called &quot;Services.&quot;
               </p>
 
               <h2 className={`text-xl font-bold text-[#8b0000] mt-4 ${merriweather.className}`}>
                 Changes to Our Privacy Policy
               </h2>
               <p>
-                We may revise this Privacy Policy from time to time in our sole discretion. If there are any material changes to this Privacy Policy, we will update the policy here and change the "Last Updated" date. It is your responsibility to review the updated Privacy Policy for changes.
+                We may revise this Privacy Policy from time to time in our sole discretion. If there are any material changes to this Privacy Policy, we will update the policy here and change the &quot;Last Updated&quot; date. It is your responsibility to review the updated Privacy Policy for changes.
               </p>
 
               <h2 className={`text-xl font-bold text-[#8b0000] mt-4 ${merriweather.className}`}>
@@ -66,7 +66,7 @@ export default function PrivacyPolicy() {
                 <li>Purchases: We may collect personal information and details associated with your purchases, including payment information. In some cases, you may need to provide us with additional information to verify your identity before completing a transaction. Any payments made via our Services are processed by third-party payment processors. We do not directly collect or store any payment card information entered through our Services, but we may receive information associated with your payment card information (e.g., your billing details).</li>
                 <li>Your Communications with Us: We may collect personal information, such as email address, phone number, or mailing address, when you request information about our Services, register for our newsletter or marketing, request customer or technical support, or otherwise communicate with us.</li>
                 <li>Surveys: We may contact you to participate in surveys. If you decide to participate, you may be asked to provide certain information, which may include personal information.</li>
-                <li>Interactive Features: We and others who use our Services may collect personal information that you submit or make available through our interactive features (e.g., collaboration tools, messaging, and chat features, commenting functionalities, forums, blogs, and social media pages). Any information you provide on the public sections of these features will be considered "public," unless otherwise required by applicable law, and is not subject to the privacy protections referenced herein.</li>
+                <li>Interactive Features: We and others who use our Services may collect personal information that you submit or make available through our interactive features (e.g., collaboration tools, messaging, and chat features, commenting functionalities, forums, blogs, and social media pages). Any information you provide on the public sections of these features will be considered &quot;public,&quot; unless otherwise required by applicable law, and is not subject to the privacy protections referenced herein.</li>
                 <li>Sweepstakes or Contests: We may collect personal information you provide for any sweepstakes or contests that we offer. In some jurisdictions, we are required to publicly share information about sweepstakes and contest winners.</li>
                 <li>Conferences, Trade Shows, and Other Events: We may collect personal information from individuals when we attend conferences, trade shows, and other events.</li>
                 <li>Business Development and Strategic Partnerships: We may collect personal information from individuals and third parties to assess and pursue potential business opportunities.</li>
@@ -79,7 +79,7 @@ export default function PrivacyPolicy() {
               <p>We may collect personal information automatically when you use our Services:</p>
               <ul className="list-disc pl-5 space-y-2">
                 <li>Automatic Data Collection: We may collect certain information automatically when you use our Services, such as your Internet protocol (IP) address, user settings, MAC address, cookie identifiers, mobile carrier, mobile advertising and other unique identifiers, browser or device information, location information (including approximate location derived from IP address), and Internet service provider. We may also automatically collect information regarding your use of our Services, such as pages that you visit before, during, and after using our Services, information about the links you click, the types of content you interact with, the frequency and duration of your activities, and other information about how you use our Services. In addition, we may collect information that other people provide about you when they use our Services, including information about you when they tag you.</li>
-                <li>Cookies, Pixel Tags/Web Beacons, and Other Technologies: We, as well as third parties that provide content, advertising, or other functionality on our Services, may use cookies, pixel tags, local storage, and other technologies ("Technologies") to automatically collect information through your use of our Services.</li>
+                <li>Cookies, Pixel Tags/Web Beacons, and Other Technologies: We, as well as third parties that provide content, advertising, or other functionality on our Services, may use cookies, pixel tags, local storage, and other technologies (&quot;Technologies&quot;) to automatically collect information through your use of our Services.</li>
                 <li>Cookies: Cookies are small text files placed in device browsers that store preferences and facilitate and enhance your experience.</li>
                 <li>Pixel Tags/Web Beacons: A pixel tag (also known as a web beacon) is a piece of code embedded in our Services that collects information about engagement on our Services. The use of a pixel tag allows us to record, for example, that a user has visited a particular web page or clicked on a particular advertisement. We may also include web beacons in emails to understand whether messages have been opened, acted on, or forwarded.</li>
               </ul>
@@ -98,14 +98,14 @@ export default function PrivacyPolicy() {
                 Analytics
               </h3>
               <p>
-                We may use Technologies and other third-party tools to process analytics information on our Services. Some of our analytics partners include Google Analytics. For more information, please visit Google Analytics' Privacy Policy. To learn more about how to opt-out of Google Analytics' use of your information on our website, please contact us.
+                We may use Technologies and other third-party tools to process analytics information on our Services. Some of our analytics partners include Google Analytics. For more information, please visit Google Analytics&apos; Privacy Policy. To learn more about how to opt-out of Google Analytics&apos; use of your information on our website, please contact us.
               </p>
 
               <h3 className={`text-lg font-semibold text-[#8b0000] mt-2 ${merriweather.className}`}>
                 Social Media Platforms
               </h3>
               <p>
-                Our Services may contain social media buttons (that might include widgets such as the "share this" button or other interactive mini programs). These features may collect your IP address, which page you are visiting on our Services, and may set a cookie to enable the feature to function properly. Your interactions with these platforms are governed by the Privacy Policy of the company providing it.
+                Our Services may contain social media buttons (that might include widgets such as the &quot;share this&quot; button or other interactive mini programs). These features may collect your IP address, which page you are visiting on our Services, and may set a cookie to enable the feature to function properly. Your interactions with these platforms are governed by the Privacy Policy of the company providing it.
               </p>
 
               <h3 className={`text-lg font-semibold text-[#8b0000] mt-2 ${merriweather.className}`}>
@@ -128,10 +128,10 @@ export default function PrivacyPolicy() {
               <p>The categories of third parties with whom we may share your information are described below:</p>
               <ul className="list-disc pl-5 space-y-2">
                 <li>Service Providers: We may share your personal information with third-party service providers who use that information to help us provide our Services. This includes service providers that assist with IT support, hosting, payment processing, customer service, and related services.</li>
-                <li>Other ShaadiToast Users at Your Direction: When you use the Services, including ShaadiToast's interactive features, you may direct ShaadiToast to share certain information with other ShaadiToast users. This information may include personal information about yourself and details about your work and your use of the Services.</li>
+                <li>Other ShaadiToast Users at Your Direction: When you use the Services, including ShaadiToast&apos;s interactive features, you may direct ShaadiToast to share certain information with other ShaadiToast users. This information may include personal information about yourself and details about your work and your use of the Services.</li>
                 <li>Business Partners: We may share your personal information with business partners to provide you with a product or service you have requested. We may also share your personal information with business partners with whom we jointly offer products or services.</li>
                 <li>Affiliates: We may share your personal information with our company affiliates for administrative purposes, including activities such as IT management, for them to provide services to you, or to support and supplement the Services we provide.</li>
-                <li>Advertising Partners: We may share your personal information with third-party advertising partners. These third-party advertising partners may set tracking technologies (such as cookies or pixel tags) on our Services to collect information regarding your activities and your device (e.g., IP address, cookie identifiers, page(s) visited, location, and time of day). These advertising partners may use this information (and similar information collected from other services) for delivering personalized advertisements to you when you visit digital properties within their networks. This practice is commonly referred to as "interest-based advertising" or "personalized advertising." Some of our advertising partners include Google Ads. For more information, please visit Google Ads' Privacy Policy. To learn more about how to opt out of Google Ads' use of your information on our website, please contact us.</li>
+                <li>Advertising Partners: We may share your personal information with third-party advertising partners. These third-party advertising partners may set tracking technologies (such as cookies or pixel tags) on our Services to collect information regarding your activities and your device (e.g., IP address, cookie identifiers, page(s) visited, location, and time of day). These advertising partners may use this information (and similar information collected from other services) for delivering personalized advertisements to you when you visit digital properties within their networks. This practice is commonly referred to as &quot;interest-based advertising&quot; or &quot;personalized advertising.&quot; Some of our advertising partners include Google Ads. For more information, please visit Google Ads&apos; Privacy Policy. To learn more about how to opt out of Google Ads&apos; use of your information on our website, please contact us.</li>
                 <li>APIs/SDKs: We may use third-party application program interfaces (APIs) and software development kits (SDKs) as part of the functionality of our Services. These APIs and SDKs may allow third parties to collect your personal information to provide and improve the Services. For more information about our use of APIs and SDKs, please contact us as set forth below.</li>
               </ul>
 
@@ -143,7 +143,7 @@ export default function PrivacyPolicy() {
               </p>
               <ul className="list-disc pl-5 space-y-2">
                 <li>Comply with law enforcement or national security requests and legal processes, such as a court order or subpoena;</li>
-                <li>Protect your, our, or others' rights, property, or safety;</li>
+                <li>Protect your, our, or others&apos; rights, property, or safety;</li>
                 <li>Enforce our policies or contracts;</li>
                 <li>Collect amounts owed to us; or</li>
                 <li>Assist with an investigation or prosecution of suspected or actual illegal activity.</li>
@@ -169,10 +169,10 @@ export default function PrivacyPolicy() {
               </p>
 
               <h3 className={`text-lg font-semibold text-[#8b0000] mt-2 ${merriweather.className}`}>
-                "Do Not Track"
+                &quot;Do Not Track&quot;
               </h3>
               <p>
-                Do Not Track ("DNT") is a privacy preference that users can set in certain web browsers. Please note that we do not respond to or honor DNT signals or similar mechanisms transmitted by web browsers.
+                Do Not Track (&quot;DNT&quot;) is a privacy preference that users can set in certain web browsers. Please note that we do not respond to or honor DNT signals or similar mechanisms transmitted by web browsers.
               </p>
 
               <h3 className={`text-lg font-semibold text-[#8b0000] mt-2 ${merriweather.className}`}>
@@ -297,10 +297,10 @@ export default function PrivacyPolicy() {
               </p>
 
               <h2 className={`text-xl font-bold text-[#8b0000] mt-4 ${merriweather.className}`}>
-                Children's Information
+                Children&apos;s Information
               </h2>
               <p>
-                The Services are not directed to children under 13 (or other age as required by local law), and we do not knowingly collect personal information from children. If you learn that your child has provided us with personal information without your consent, you may contact us as set forth below. If we learn that we have collected a child's personal information in violation of applicable law, we will promptly take steps to delete such information.
+                The Services are not directed to children under 13 (or other age as required by local law), and we do not knowingly collect personal information from children. If you learn that your child has provided us with personal information without your consent, you may contact us as set forth below. If we learn that we have collected a child&apos;s personal information in violation of applicable law, we will promptly take steps to delete such information.
               </p>
 
               <h2 className={`text-xl font-bold text-[#8b0000] mt-4 ${merriweather.className}`}>
@@ -321,12 +321,12 @@ export default function PrivacyPolicy() {
                 Contact Us
               </h2>
               <p>
-                If you have any questions about our privacy practices or this Privacy Policy, or to exercise your rights as detailed in this Privacy Policy, please contact us at:
+               If you have any questions about our privacy practices or this Privacy Policy, or to exercise your rights as detailed in this Privacy Policy, please contact us at:
               </p>
               <p className="mt-2">
                 ShaadiToast<br />
                 2625 Middlefield Rd #435<br />
-                Palo Alto, CA 94306<br />
+                PaloAlto, CA 94306<br />
                 shaaditoast@greenpenailabs.com
               </p>
             </div>
